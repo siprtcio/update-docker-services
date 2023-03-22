6c8883542c4941ab5f53a2e82e4d41b26811e07a
Shail <shail@tiniyo.com>
2023-03-22 12:56:21 +0530

    fix

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
@@ -37,17 +40,42 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Loop through all services and update their image
+	// Authenticate with Amazon ECR
+	authConfig := types.AuthConfig{
+		Username: os.Getenv("AWS_ACCESS_KEY_ID"),
+		Password: os.Getenv("AWS_SECRET_ACCESS_KEY"),
+	}
+	authConfigBytes, err := json.Marshal(authConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	authConfigEncoded := base64.URLEncoding.EncodeToString(authConfigBytes)
+
+	// Loop through all services and pull the new image to all swarm nodes
 	for _, service := range services {
 		// Get the current service configuration
 		currentSpec := service.Spec
+
+		// Pull the new image from Amazon ECR to all nodes in the swarm
+		image := strings.Split(newImageName, ":")[0]
+		tag := strings.Split(newImageName, ":")[1]
+		imagePullResponse, err := cli.ImagePull(context.Background(), fmt.Sprintf("%s:%s", image, tag), types.ImagePullOptions{
+			RegistryAuth: authConfigEncoded,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer imagePullResponse.Close()
+
+		// Print a message indicating that the image was pulled to the swarm nodes
+		fmt.Printf("Image %s pulled to all nodes in service %s\n", newImageName, service.Spec.Name)
 
 		// Update the image in the service configuration
 		newSpec := currentSpec
 		newSpec.TaskTemplate.ContainerSpec.Image = newImageName
 
 		// Update the service with the new configuration
-		_, err := cli.ServiceUpdate(context.Background(), service.ID, service.Version, newSpec, types.ServiceUpdateOptions{})
+		_, err = cli.ServiceUpdate(context.Background(), service.ID, service.Version, newSpec, types.ServiceUpdateOptions{})
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -55,10 +83,4 @@ func main() {
 		// Print a message indicating that the service was updated
 		fmt.Printf("Service %s updated to use image %s\n", service.Spec.Name, newImageName)
 	}
-}
-
-// Helper function to get the Docker stack namespace
-func getStackNamespace(stackName string) string {
-	namespace := stackName
-	return namespace
 }
